b51352af9af31e961225b192ec35894b40d8a5d9
Chris Kelner <ckelner@users.noreply.github.com>
2017-02-20 11:42:58 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,68 @@
-resource "bluemix_watson_visual_recognition" "watson-vr" {
+// ####################
+// ## Object Storage ##
+// ####################
+resource "bluemix_object_storage_bucket" "input_bucket" {
+  bucket = "${var.composite_name}-input"
+  acl = "private"
+  tags {
+    Name = "Darkvision Video Stills Input"
+  }
+  versioning {
+    enabled = false
+  }
+  lifecycle_rule {
+    enabled = true
+    prefix = ""
+    noncurrent_version_expiration {
+      days = "5"
+    }
+  }
+}
+
+resource "bluemix_object_storage_bucket" "content_bucket" {
+  bucket = "${var.composite_name}-content"
+  acl = "private"
+  tags {
+    Name = "Darkvision Video Stills Content"
+  }
+  versioning {
+    enabled = false
+  }
+  lifecycle_rule {
+    enabled = true
+    prefix = ""
+    noncurrent_version_expiration {
+      days = "5"
+    }
+  }
+}
+
+
+resource "bluemix_object_storage_bucket" "thumbnail_bucket" {
+  bucket = "${var.composite_name}-thumbnails"
+  acl = "private"
+  tags {
+    Name = "Darkvision Video Stills Thumbnails"
+  }
+  versioning {
+    enabled = false
+  }
+  lifecycle_rule {
+    enabled = true
+    prefix = ""
+    noncurrent_version_expiration {
+      days = "5"
+    }
+  }
+}
+
+
+// #######################################
+// ## Watson Visual Recognition Service ##
+// #######################################
+resource "bluemix_watson_visual_recognition" "watson_vr" {
   input_bucket = "${bluemix_object_storage.input_bucket.bucket}"
-  name         = "darkvision_visual-recognition"
+  name         = "${var.composite_name}"
 
   content_config = {
     bucket        = "${bluemix_object_storage.content_bucket.bucket}"
@@ -8,15 +70,15 @@ resource "bluemix_watson_visual_recognition" "watson-vr" {
   }
 
   thumbnail_config = {
-    bucket        = "${bluemix_object_storage.thumb_bucket.bucket}"
+    bucket        = "${bluemix_object_storage.thumbnail_bucket.bucket}"
     storage_class = "Standard"
   }
 }
 
-resource "bluemix_watson_visual_recognition_preset" "watson-vr-preset" {
+resource "bluemix_watson_visual_recognition_preset" "watson_vr_preset" {
   container   = "mp4"
   description = "Darkvision Watson Visual Recognition"
-  name        = "darkvision_visual-recognition-preset"
+  name        = "${var.composite_name}"
 
   audio = {
     audio_packing_mode = "SingleTrack"
@@ -73,4 +135,61 @@ resource "bluemix_watson_visual_recognition_preset" "watson-vr-preset" {
     padding_policy = "Pad"
     sizing_policy  = "Fit"
   }
+}
+
+// ###############
+// ## OpenWhisk ##
+// ###############
+
+resource "bluemix_iam_role" "openwhisk" {
+  name = "${var.composite_name}"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "openwhisk.bluemix.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+data "bluemix_iam_policy_document" "create_video_stills" {
+  statement {
+    sid = "1"
+    actions = [
+      "bluemix:object_store:*"
+    ]
+    resources = ["*"]
+  }
+}
+
+data "archive_file" "openwhisk_function" {
+  type        = "zip"
+  source_file = "https://github.com/IBM-Bluemix/openwhisk-darkvisionapp/blob/master/processing/extractor/client/extract.js"
+  output_path = "./function-archive.zip"
+}
+
+resource "bluemix_iam_role_policy" "openwhisk" {
+  name = "${var.composite_name}"
+  role = "${bluemix_iam_role.openwhisk.name}"
+
+  policy = "${data.bluemix_iam_policy_document.create_video_stills}"
+}
+
+resource "bluemix_openwhisk_function" "video-stills-function" {
+  runtime          = "nodejs4.3"
+  filename         = "./function-archive.zip"
+  function_name    = "${var.composite_name}_stills-func"
+  role             = "${bluemix_iam_role.openwhisk.crn}"
+  handler          = "createDocument"
+  source_code_hash = "${data.archive_file.openwhisk_function.output_base64sha256}"
+  count            = "${var.enabled}"
 }
