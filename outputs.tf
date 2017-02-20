bc8a307578589e03a84041148e435b52ec8f0554
Chris Kelner <ckelner@users.noreply.github.com>
2017-02-20 11:42:11 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,15 @@
-output "lambda_arn" {
-  value = "${aws_lambda_function.lambda.arn}"
+output "openwhisk_crn" {
+  value = "${aws_lambda_function.lambda.crn}"
+}
+
+output "input_bucket_crn" {
+  value = "${bluemix_object_storage_bucket.input_bucket.crn}"
+}
+
+output "content_bucket_crn" {
+  value = "${bluemix_object_storage_bucket.content_bucket.crn}"
+}
+
+output "thumbnail_bucket_crn" {
+  value = "${bluemix_object_storage_bucket.thumbnail_bucket.crn}"
 }
